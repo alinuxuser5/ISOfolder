494dc3a4aae784e2ae32baf917794bb47c1b137a
alinuxuser5 <147749687+alinuxuser5@users.noreply.github.com>
2023-10-13 11:26:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,16 +8,17 @@ sudo touch /usr/share/isofolder/log/inst1-log.log
 echo "inst=inst1" | sudo tee /usr/share/isofolder/log/inst1-log.log
 sudo cp isofolder-files/ /usr/share/isofolder -r
 sudo cp /usr/share/images/vendor-logos/logo.svg /usr/share/isofolder/images/logo-`uname -n`-`uname -m`.svg
+sudo chmod +x script.sh
 sudo chmod u+x script.sh
+sudo chmod 766 script.sh
+sudo chmod +rwx script.sh
 sudo cp script.sh /usr/share/isofolder/
 sudo cp script.sh /usr/share/isofolder/spot/script.sh.spot
 sudo cp isofolder-files/org.alinuxproject5.isofolder.desktop /usr/share/applications
 sudo touch /usr/share/isofolder/installation.txt
 echo "Install" | sudo tee /usr/share/isofolder/installation.txt
 read -p "What's your username? " USERNAME_INSTALLATION
-cd /usr/share/isofolder
-sudo wget https://github.com/alinuxuser5/ISOfolder/archive/refs/heads/main.zip
-cd /home/"$USERNAME_INSTALLATION"
+sudo wget https://github.com/alinuxuser5/ISOfolder/archive/refs/heads/main.zip -P /usr/share/isofolder
 sudo touch /usr/share/isofolder/EMPTY
 sudo cp isofolder-files/org.alinuxproject5.isofolder.desktop /home/"$USERNAME_INSTALLATION"/Desktop
 sudo cp isofolder-files/org.alinuxproject5.isofolder.desktop /usr/share/isofolder/spot/desktop.spot
