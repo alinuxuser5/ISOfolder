f8d5e64017135384c2189701bad5448c26876777
alinuxuser5 <147749687+alinuxuser5@users.noreply.github.com>
2023-10-13 11:31:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+echo "Making directories..."
 sudo mkdir /usr/share/isofolder
 sudo mkdir /usr/share/isofolder/spot
 sudo mkdir /usr/share/isofolder/log
 sudo mkdir /usr/share/isofolder/images
+echo "Making files..."
 sudo touch /usr/share/isofolder/log/inst1-log.log
 echo "inst=inst1" | sudo tee /usr/share/isofolder/log/inst1-log.log
 sudo cp isofolder-files/ /usr/share/isofolder -r
@@ -17,10 +19,10 @@ sudo cp script.sh /usr/share/isofolder/spot/script.sh.spot
 sudo cp isofolder-files/org.alinuxproject5.isofolder.desktop /usr/share/applications
 sudo touch /usr/share/isofolder/installation.txt
 echo "Install" | sudo tee /usr/share/isofolder/installation.txt
-read -p "What's your username? " USERNAME_INSTALLATION
+echo "Installing resources..."
 sudo wget https://github.com/alinuxuser5/ISOfolder/archive/refs/heads/main.zip -P /usr/share/isofolder
 sudo touch /usr/share/isofolder/EMPTY
-sudo cp isofolder-files/org.alinuxproject5.isofolder.desktop /home/"$USERNAME_INSTALLATION"/Desktop
+sudo cp isofolder-files/org.alinuxproject5.isofolder.desktop /home/`whoami`/Desktop
 sudo cp isofolder-files/org.alinuxproject5.isofolder.desktop /usr/share/isofolder/spot/desktop.spot
 sudo touch /usr/share/isofolder/spot/official.spot
 echo "spot=desktop.spot,script.sh.spot" | sudo tee /usr/share/isofolder/spot/official.spot
