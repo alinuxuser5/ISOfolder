ecd6d63955d9933cbbd3e63d85b50ee0655823cf
alinuxuser5 <147749687+alinuxuser5@users.noreply.github.com>
2023-10-13 10:56:21 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-su root
-mkdir /usr/share/isofolder
-mkdir /usr/share/isofolder/spot
-mkdir /usr/share/isofolder/log
-mkdir /usr/share/isofolder/images
-touch /usr/share/isofolder/log/inst1-log.log
-echo "${whoami}=inst1" | tee /usr/share/isofolder/log/inst1-log.log
-cp isofolder-files/ /usr/share/isofolder -r
-cp /usr/share/images/vendor-logos/logo.svg /usr/share/isofolder/images/logo-`uname -n`-`uname -m`.svg
-chmod u+x script.sh
-cp script.sh /usr/share/isofolder/
-cp script.sh /usr/share/isofolder/spot/script.sh.spot
-cp isofolder-files/org.alinuxproject5.isofolder.desktop /usr/share/applications
-touch /usr/share/isofolder/installation.txt
-echo "Installed as ${whoami}." | tee /usr/share/isofolder/installation.txt
+sudo mkdir /usr/share/isofolder
+sudo mkdir /usr/share/isofolder/spot
+sudo mkdir /usr/share/isofolder/log
+sudo mkdir /usr/share/isofolder/images
+sudo touch /usr/share/isofolder/log/inst1-log.log
+echo "${whoami}=inst1" | sudo tee /usr/share/isofolder/log/inst1-log.log
+sudo cp isofolder-files/ /usr/share/isofolder -r
+sudo cp /usr/share/images/vendor-logos/logo.svg /usr/share/isofolder/images/logo-`uname -n`-`uname -m`.svg
+sudo chmod u+x script.sh
+sudo cp script.sh /usr/share/isofolder/
+sudo cp script.sh /usr/share/isofolder/spot/script.sh.spot
+sudo cp isofolder-files/org.alinuxproject5.isofolder.desktop /usr/share/applications
+sudo touch /usr/share/isofolder/installation.txt
+echo "Installed as ${whoami}." | sudo tee /usr/share/isofolder/installation.txt
 read -p "What's your username? " USERNAME_INSTALLATION
 cd /usr/share/isofolder
-wget https://github.com/alinuxuser5/ISOfolder/archive/refs/heads/main.zip
+sudo wget https://github.com/alinuxuser5/ISOfolder/archive/refs/heads/main.zip
 cd /home/"$USERNAME_INSTALLATION"
-touch /usr/share/isofolder/EMPTY
-cp isofolder-files/org.alinuxproject5.isofolder.desktop /home/"$USERNAME_INSTALLATION"/Desktop
-cp isofolder-files/org.alinuxproject5.isofolder.desktop /usr/share/isofolder/spot/desktop.spot
-touch /usr/share/isofolder/spot/official.spot
-echo "spot=desktop.spot,script.sh.spot" | tee /usr/share/isofolder/spot/official.spot
-exit
+sudo touch /usr/share/isofolder/EMPTY
+sudo cp isofolder-files/org.alinuxproject5.isofolder.desktop /home/"$USERNAME_INSTALLATION"/Desktop
+sudo cp isofolder-files/org.alinuxproject5.isofolder.desktop /usr/share/isofolder/spot/desktop.spot
+sudo touch /usr/share/isofolder/spot/official.spot
+echo "spot=desktop.spot,script.sh.spot" | sudo tee /usr/share/isofolder/spot/official.spot
 echo "Installation completed. You can now open this program on Application Menu."
