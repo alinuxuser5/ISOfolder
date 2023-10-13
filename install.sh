e80dcc0e9da631faa6ee52e1368db081b7711f1d
alinuxuser5 <147749687+alinuxuser5@users.noreply.github.com>
2023-10-13 10:59:55 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo mkdir /usr/share/isofolder/spot
 sudo mkdir /usr/share/isofolder/log
 sudo mkdir /usr/share/isofolder/images
 sudo touch /usr/share/isofolder/log/inst1-log.log
-echo "${whoami}=inst1" | sudo tee /usr/share/isofolder/log/inst1-log.log
+echo "inst=inst1" | sudo tee /usr/share/isofolder/log/inst1-log.log
 sudo cp isofolder-files/ /usr/share/isofolder -r
 sudo cp /usr/share/images/vendor-logos/logo.svg /usr/share/isofolder/images/logo-`uname -n`-`uname -m`.svg
 sudo chmod u+x script.sh
@@ -13,7 +13,7 @@ sudo cp script.sh /usr/share/isofolder/
 sudo cp script.sh /usr/share/isofolder/spot/script.sh.spot
 sudo cp isofolder-files/org.alinuxproject5.isofolder.desktop /usr/share/applications
 sudo touch /usr/share/isofolder/installation.txt
-echo "Installed as ${whoami}." | sudo tee /usr/share/isofolder/installation.txt
+echo "Install" | sudo tee /usr/share/isofolder/installation.txt
 read -p "What's your username? " USERNAME_INSTALLATION
 cd /usr/share/isofolder
 sudo wget https://github.com/alinuxuser5/ISOfolder/archive/refs/heads/main.zip
