57dc22af24d5d5d5972a2583cc0a0eaad2fdc4b2
alinuxuser5 <147749687+alinuxuser5@users.noreply.github.com>
2023-10-15 04:10:56 -0400

    Create JavaData1.java

---
+++ isofolder-files/data/java/JavaData1.java
@@ -0,0 +1,7 @@
+public class JavaData1 {
+
+    public static void main(String[] args) {
+        // Your Java data and operations go here
+        System.out.println("Java data and operations");
+    }
+}
