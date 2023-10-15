5a8517608374e51dbb00f2f6dc1c882cda474989
alinuxuser5 <147749687+alinuxuser5@users.noreply.github.com>
2023-10-15 04:07:44 -0400

    Create data-readme.go

---
+++ isofolder-files/data/go/data-readme.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Data README for my application
+
+    fmt.Println("This Go file serves as a README for the data in my application.")
+    fmt.Println("It includes information about the data structures, formats, and usage.")
+    fmt.Println("Feel free to add and update the data-related details here.")
+}
